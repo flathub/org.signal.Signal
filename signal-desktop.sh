1cb5acae8049f509ed0488711da3c37415d31c34
Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>
2022-04-10 10:24:56 +0200

    Cleanup Wayland

--- signal-desktop.sh
+++ signal-desktop.sh
@@ -2,39 +2,36 @@
 
 EXTRA_ARGS=()
 
-if [[ -z "${DISPLAY}" ]] && [[ -n "${WAYLAND_DISPLAY}" ]];
-then
+# Special treatment for Wayland
+if [[ "${XDG_SESSION_TYPE}" == "wayland" ]]; then
     EXTRA_ARGS+=(
-        "--enable-features=UseOzonePlatform"
         "--ozone-platform=wayland"
-        "--disable-gpu"
+        "--enable-features=UseOzonePlatform"
         "--enable-features=WaylandWindowDecorations"
+        "--disable-gpu"
     )
-fi
 
-if [[ "${XDG_SESSION_TYPE}" == "wayland" ]] && [[ -c /dev/nvidia0 ]];
-then
-    EXTRA_ARGS+=(
-        "--disable-gpu-sandbox"
+    if [[ -c /dev/nvidia0 ]]; then
+        EXTRA_ARGS+=(
+            "--disable-gpu-sandbox"
     )
+    fi
 fi
 
 # Additional args for tray icon
-if [[ -n "${SIGNAL_USE_TRAY_ICON+x}" ]];
-then
+if [[ -n "${SIGNAL_USE_TRAY_ICON+x}" ]]; then
     EXTRA_ARGS+=(
         "--use-tray-icon"
     )
 fi
-if [[ -n "${SIGNAL_START_IN_TRAY+x}" ]];
-then
+if [[ -n "${SIGNAL_START_IN_TRAY+x}" ]]; then
     EXTRA_ARGS+=(
         "--start-in-tray"
     )
 fi
 
-
-
+echo "Debug: Will run signal with the following arguments: ${EXTRA_ARGS[@]}"
+echo "Debug: Additionally, user gave: $@"
 
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 exec zypak-wrapper /app/Signal/signal-desktop "${EXTRA_ARGS[@]}" "$@"
