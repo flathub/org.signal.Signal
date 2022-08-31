4e7b9455f3c11f4e02761006870a05c2600b5a27
joshua-stone <joshua.gage.stone@gmail.com>
2022-08-31 11:45:45 -0400

    Have flatpak set environment variables for greater visability (#343)
    The Signal launcher script checks for numerous variables to pass
    CLI arguments. This change has them set directly by flatpak so
    that they can be visible in frontends like Flatseal. This change
    also switches from checking for set variables to instead test
    numerical values which is generally more consistent in Bash.

--- signal-desktop.sh
+++ signal-desktop.sh
@@ -2,32 +2,38 @@
 
 EXTRA_ARGS=()
 
+declare -i SIGNAL_USE_TRAY_ICON="${SIGNAL_USE_TRAY_ICON:-0}"
+declare -i SIGNAL_START_IN_TRAY="${SIGNAL_START_IN_TRAY:-0}"
+declare -i SIGNAL_USE_WAYLAND="${SIGNAL_USE_WAYLAND:-0}"
+declare -i SIGNAL_DISABLE_GPU="${SIGNAL_DISABLE_GPU:-0}"
+declare -i SIGNAL_DISABLE_GPU_SANDBOX="${SIGNAL_DISABLE_GPU_SANDBOX:-0}"
+
 # Additional args for tray icon
-if [[ -n "${SIGNAL_USE_TRAY_ICON+x}" ]]; then
+if [[ "${SIGNAL_USE_TRAY_ICON}" -eq 1 ]]; then
     EXTRA_ARGS+=(
         "--use-tray-icon"
     )
 fi
-if [[ -n "${SIGNAL_START_IN_TRAY+x}" ]]; then
+if [[ "${SIGNAL_START_IN_TRAY}" -eq 1 ]]; then
     EXTRA_ARGS+=(
         "--start-in-tray"
     )
 fi
 
-if [[ -n "${SIGNAL_USE_WAYLAND+x}" && "${XDG_SESSION_TYPE}" == "wayland" ]]; then
+if [[ "${SIGNAL_USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE}" == "wayland" ]]; then
     EXTRA_ARGS+=(
         "--enable-features=WaylandWindowDecorations"
         "--ozone-platform=wayland"
     )
 fi
 
-if [[ -n "${SIGNAL_DISABLE_GPU+x}" ]]; then
+if [[ "${SIGNAL_DISABLE_GPU}" -eq 1 ]]; then
     EXTRA_ARGS+=(
         "--disable-gpu"
     )
 fi
 
-if [[ -n "${SIGNAL_DISABLE_GPU_SANDBOX+x}" ]]; then
+if [[ "${SIGNAL_DISABLE_GPU_SANDBOX}" -eq 1 ]]; then
     EXTRA_ARGS+=(
         "--disable-gpu-sandbox"
     )
