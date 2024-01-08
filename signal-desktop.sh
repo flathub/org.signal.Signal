f56d6e1bec97f57df5c4704793513b0be326990c
Bernhard Bermeitinger <bernhard.bermeitinger@gmail.com>
2024-01-08 18:01:49 +0100

    Revert "Enable automatic selection of Wayland/X11 (#572)"
    This reverts commit 6d13de84f0710f6153679816df0cf6d7a4c80b4c.

--- signal-desktop.sh
+++ signal-desktop.sh
@@ -4,6 +4,7 @@ EXTRA_ARGS=()
 
 declare -i SIGNAL_USE_TRAY_ICON="${SIGNAL_USE_TRAY_ICON:-0}"
 declare -i SIGNAL_START_IN_TRAY="${SIGNAL_START_IN_TRAY:-0}"
+declare -i SIGNAL_USE_WAYLAND="${SIGNAL_USE_WAYLAND:-0}"
 declare -i SIGNAL_DISABLE_GPU="${SIGNAL_DISABLE_GPU:-0}"
 declare -i SIGNAL_DISABLE_GPU_SANDBOX="${SIGNAL_DISABLE_GPU_SANDBOX:-0}"
 
@@ -16,6 +17,13 @@ fi
 if [[ "${SIGNAL_START_IN_TRAY}" -eq 1 ]]; then
     EXTRA_ARGS+=(
         "--start-in-tray"
+    )
+fi
+
+if [[ "${SIGNAL_USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE}" == "wayland" ]]; then
+    EXTRA_ARGS+=(
+        "--enable-features=WaylandWindowDecorations"
+        "--ozone-platform=wayland"
     )
 fi
 
@@ -36,4 +44,4 @@ echo "Debug: Will run signal with the following arguments: ${EXTRA_ARGS[@]}"
 echo "Debug: Additionally, user gave: $@"
 
 export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
-exec zypak-wrapper /app/Signal/signal-desktop --ozone-platform-hint=auto "${EXTRA_ARGS[@]}" "$@"
+exec zypak-wrapper /app/Signal/signal-desktop "${EXTRA_ARGS[@]}" "$@"
