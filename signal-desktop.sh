00787d19e293e124bb4c9f2ffa3083470d5b213b
Cedric Staniewski <cedric@gmx.ca>
2024-02-13 22:11:07 +0100

    Simplify logic to enable wayland ozone backend
    This uses the new environment variable `ELECTRON_OZONE_PLATFORM_HINT` [1],
    which is available since electron v28 [2].
    
    [1] https://www.electronjs.org/docs/latest/api/environment-variables#electron_ozone_platform_hint-linux
    [2] https://github.com/electron/electron/pull/39792

--- signal-desktop.sh
+++ signal-desktop.sh
@@ -2,14 +2,11 @@
 
 EXTRA_ARGS=()
 
-declare -i SIGNAL_USE_WAYLAND="${SIGNAL_USE_WAYLAND:-0}"
 declare -i SIGNAL_DISABLE_GPU="${SIGNAL_DISABLE_GPU:-0}"
 declare -i SIGNAL_DISABLE_GPU_SANDBOX="${SIGNAL_DISABLE_GPU_SANDBOX:-0}"
 
-if [[ "${SIGNAL_USE_WAYLAND}" -eq 1 && "${XDG_SESSION_TYPE}" == "wayland" ]]; then
-    EXTRA_ARGS+=(
-        "--ozone-platform=wayland"
-    )
+if (( ${SIGNAL_USE_WAYLAND:-0} )); then
+    export ELECTRON_OZONE_PLATFORM_HINT="${ELECTRON_OZONE_PLATFORM_HINT:-auto}"
 fi
 
 if [[ "${SIGNAL_DISABLE_GPU}" -eq 1 ]]; then
