ec62bbb94f74629adbbd5d41c100f9e027d03d33
Bernhard Bermeitinger <bernhard.bermeitinger@gmail.com>
2024-09-27 12:50:41 +0200

    Fix spelling gnome-libsecret
    Signed-off-by: Bernhard Bermeitinger <bernhard.bermeitinger@gmail.com>

--- signal-desktop.sh
+++ signal-desktop.sh
@@ -7,7 +7,7 @@ default due to database corruption bugs when using the encrypted backends.
 This will leave your keys <b>unencrypted</b> on disk as it did in all previous versions.
 
 If you wish to experiment with the encrypted backend, set the environment variable
-<tt>SIGNAL_PASSWORD_STORE</tt> to <tt>gnome_libsecret</tt>, <tt>kwallet</tt>,
+<tt>SIGNAL_PASSWORD_STORE</tt> to <tt>gnome-libsecret</tt>, <tt>kwallet</tt>,
 <tt>kwallet5</tt> or <tt>kwallet6</tt> depending on your desktop environment using
 Flatseal or the following command:
 
